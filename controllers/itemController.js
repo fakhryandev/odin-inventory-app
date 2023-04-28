7c3ebbc092a175232089ea9df3881916b42870fe
Muhammad Fakhryan <fakhryandev@gmail.com>
2023-04-28 23:02:09 +0700

    item list

--- controllers/itemController.js
+++ controllers/itemController.js
@@ -1,12 +1,19 @@
-const categories = ['Makanan', 'Alat Masak']
-const items = ['Bakso', 'Spatula', 'Wajan']
+const categories = ["Makanan", "Alat Masak"];
+const items = ["Bakso", "Spatula", "Wajan"];
 
 exports.index = (req, res) => {
-    res.render('index', {
-        title: 'My Inventory Home',
-        data: {
-            categories,
-            items
-        }
-    })
-}
+  res.render("index", {
+    title: "My Inventory Home",
+    data: {
+      categories,
+      items,
+    },
+  });
+};
+
+exports.item_list = (req, res, next) => {
+  res.render("item_list", {
+    title: "Item List",
+    item_list: items,
+  });
+};
--- routes/inventory.js
+++ routes/inventory.js
@@ -4,6 +4,7 @@ const itemController = require("../controllers/itemController");
 const categoryController = require("../controllers/categoryController");
 
 router.get("/", itemController.index);
+router.get("/item", itemController.item_list);
 
 router.get("/category", categoryController.category_list);
 
