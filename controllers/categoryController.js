67d99d87c5a7efd407c8b98825caeb40211742ec
Muhammad Fakhryan <fakhryandev@gmail.com>
2023-04-28 23:40:12 +0700

    category detail

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -1,8 +1,48 @@
-const categories = ["Makanan", "Alat Masak"];
+const categories = [
+  {
+    id: 1,
+    name: "Makanan",
+    description: "Kumpulan makanan dengan gizi tinggi",
+  },
+  {
+    id: 2,
+    name: "Alat Masak",
+    description: "Kumpulan alat masak dengan kualitas terbaik",
+  },
+];
+
+const items = [
+  {
+    name: "Bakso",
+    description: "Cita rasa bakso malang dengan daging sapi pilihan",
+    category: 1,
+    price: 20,
+    stock: 5,
+  },
+  {
+    name: "Wajan",
+    description: "Berbahan stainless steel kelas eropa",
+    category: 2,
+    price: 30,
+    stock: 10,
+  },
+];
 
 exports.category_list = (req, res, next) => {
   res.render("category_list", {
     title: "Category List",
     category_list: categories,
+  });
+};
+
+exports.category_detail = (req, res) => {
+  const id = req.params.id;
+  const category = categories.find((x) => x.id == id);
+  const itemByCategory = items.filter((x) => x.category == id);
+
+  res.render("category_detail", {
+    title: "Category Detail",
+    category,
+    category_item: itemByCategory,
   });
 };
--- routes/inventory.js
+++ routes/inventory.js
@@ -7,5 +7,6 @@ router.get("/", itemController.index);
 router.get("/item", itemController.item_list);
 
 router.get("/category", categoryController.category_list);
+router.get("/category/:id", categoryController.category_detail);
 
 module.exports = router;
